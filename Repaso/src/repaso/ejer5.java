c14b2284df6a9a0caf310f77bf8da4447ee5c79c
Virginia <virginia_ortiz_rodriguez@hotmail.com>
2022-01-21 09:56:22 +0100

    comit inicial

---
+++ Repaso/src/repaso/ejer5.java
@@ -0,0 +1,26 @@
+package repaso;
+
+import java.util.Scanner;
+
+public class ejer5 {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("introduce un texto");
+		String cadena = entrada.nextLine();
+
+		System.out.println("Introduce el caracter a buscar: ");
+		char letra = entrada.nextLine().charAt(0);
+		int contador=0;
+		
+		
+		
+			
+		}
+		
+
+
+	}
+
+
